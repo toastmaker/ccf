ecb91d207371abe3c52cf5c4173890406686c06b
Filip Munz <limu007@gmail.com>
2020-09-03 18:13:16 +0200

    lcurve generation finally corrected

--- notebooks/evlist_corr.py
+++ notebooks/evlist_corr.py
@@ -1,5 +1,37 @@
 import numpy as np
 
+def clipped(val,nsig=3.,nclip=1,loud=0,rep=0):
+    '''clipped average'''
+    from numpy import bool,ones
+    sel=ones(val.shape,bool)
+    rcheck=False
+    nold=len(val)
+    if nclip<0: 
+        nclip=nold
+        rcheck=True
+    for i in range(nclip):
+        avg=val[sel].mean()
+        std=val[sel].std()
+        if loud>0: print("selecting %f +- %f"%(avg,std*nsig))
+        sel=abs(val-avg)<std*nsig
+        if rcheck:
+            nnew=sum(sel)
+            if nnew==nold: break # no sense of repeating
+            nold=nnew
+    if rep==1: return val[sel].min(),val[sel].max()
+    return sel
+
+def clipperc(st,ran=[10,90]):
+    # reject lowest and highest data points
+    low,hig=np.percentile(st,ran)
+    return st[(st>=low)*(st<hig)]
+
+#vals=gres2[:,2].reshape(nshi,subsam**2)[3]
+def randids(z0=0,z1=100,frac=5):
+    slen=(z1-z0)//frac
+    return z0+np.random.randint(frac,size=slen)+frac*np.arange(slen)
+#clipped(vals,nclip=3)
+
 def lcurve(time,fac=600,sub=0,tstart=0):
     #fac: grouping factor - binning = 1/fac seconds
     tlab=(time*fac).astype(int)
@@ -8,12 +40,16 @@ def lcurve(time,fac=600,sub=0,tstart=0):
     if sub!=0: #baseline
         basel=np.median(cnt[sub:])
         cnt=cnt.astype(float)-basel
-    tbin=np.r_[tlab[0]:tlab[0]+len(cnt)]/fac
+    tbin=np.r_[tlab[0]:tlab[-1]+1]/fac
+    cur=np.zeros_like(tbin)
+    #cur[pos[1:-1]]=cnt[:-1]
+    cur[tlab[pos[1:]]-tlab[0]]=cnt
+    #tbin=tlab[pos[1:]]/fac
     if tstart>0:
         tsel=tbin>tstart
         tbin=tbin[tsel]
-        cnt=cnt[tsel]
-    return tbin,cnt*fac
+        cur=cur[tsel]
+    return tbin,cur*fac
 
 def wcent(cfunc,nbin=10,sbin=20,cent=0):
     if cent==0: cent=len(cfunc)//2
@@ -41,3 +77,13 @@ def fit_corr(cfunc,csiz=200,doplot=True,inifac=1,cmax=0):
     #pl.plot(x,fun1(p0))
     chi2=((cfunc[cmax-csiz:cmax+csiz]-fun1(bst))**2).sum()
     return list(bst)+[chi2]
+
+def tcdf(z,pf,pc=[0.1,0.9]):
+    #truncated normal CDF
+    from scipy import stats as st
+    from numpy import iterable
+    vals=(st.norm(pf[0],pf[1]).cdf(z)-pc[0])/(pc[1]-pc[0])
+    if iterable(vals):
+        vals[vals<0]=0
+        vals[vals>1]=1
+    return vals
